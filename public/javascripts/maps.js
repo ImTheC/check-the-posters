19f8c7522e463a18732876cbc2044c72cb930eea
Christopher Anderson <Christopher@ImTheC.com>
2016-06-30 13:30:53 -0500

    Add edit\delete functions and make it look good

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -1,22 +1,17 @@
 function initMap() {
 	var map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 8,
-		center: {lat: 30.267153, lng: -97.74306079999997}
+		center: {lat: 30.267153, lng: -97.74306079999997} // Austin, TX
 	});
 
 	var geocoder = new google.maps.Geocoder();
-	document.getElementById('address').addEventListener('focusout', function() {
-		geocodeAddress(geocoder, map);
-	});
-	document.getElementById('address').addEventListener('keyup', function(key) {
-		if ( key === 13) {
-			geocodeAddress(geocoder, map);
-		}
-	});
+	var address = document.getElementById('address').value;
+	geocodeAddress(geocoder, map, address);
+
 }
 
-function geocodeAddress(geocoder, resultsMap) {
-	var address = document.getElementById('address').value;
+function geocodeAddress(geocoder, resultsMap, address) {
+	console.log(address);
 	geocoder.geocode({'address': address}, function(results, status) {
 		if (status === google.maps.GeocoderStatus.OK) {
 
@@ -28,12 +23,9 @@ function geocodeAddress(geocoder, resultsMap) {
 
 			resultsMap.setCenter(results[0].geometry.location);
 			resultsMap.setZoom(15);
-			document.getElementById('firstComponent').innerHTML="The Formatted Address is: " + results[0].formatted_address;
-			document.getElementById('secondComponent').innerHTML="The Location is " + results[0].geometry.location;
 
 		} else {
 			alert('Geocode was not successful for the following reason: ' + status);
 		}
 	});
-
 }
