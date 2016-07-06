bac0d3afdc49eb5a73e427f9a2d7782d5363fd32
Christopher Anderson <Christopher@ImTheC.com>
2016-07-06 00:38:24 -0500

    Make subnav more mobile responsive

--- public/javascripts/mobile-menu-init.js
+++ public/javascripts/mobile-menu-init.js
@@ -1,3 +1,6 @@
 $(function(){
-	$(".button-collapse").sideNav();
+	$(".button-collapse").sideNav({
+      menuWidth: 150, // Default is 240
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    });
 }); // End of jQuery
